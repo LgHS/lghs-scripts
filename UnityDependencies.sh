db9b06fc1157fad7cbf1c86081c3b538dd534ddf
Arthur Wallendorff <flacodevworks@gmail.com>
2017-08-19 22:54:52 +0200

    Adding /bin/bash reference + correcting errors in comment

--- UnityDependencies.sh
+++ UnityDependencies.sh
@@ -1,2 +1,4 @@
-# Generaland WebGL dependencies and recommended packages
+#!bin/bash
+
+# General and WebGL dependencies/recommended packages
 apt-get install gconf-service lib32gcc1 lib32stdc++6 libasound2 libc6 libc6-i386 libcairo2 libcap2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libfreetype6 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libgl1-mesa-glx libgl1 libglib2.0-0 libglu1-mesa libglu1 libgtk2.0-0 libnspr4 libnss3 libpango1.0-0 libstdc++6 libx11-6 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxtst6 zlib1g debconf npm ffmpeg libav-tools nodejs gzip
