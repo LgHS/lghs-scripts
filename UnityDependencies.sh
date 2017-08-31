eb3c3136bee66d4b5b1385d2752d7d81ccb0ba94
Arthur Wallendorff <flacodevworks@gmail.com>
2017-08-31 14:27:34 +0200

    Removed some useless packages

--- UnityDependencies.sh
+++ UnityDependencies.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 
+# Installing Unity3D's dependencies, based on this forum post : https://forum.unity3d.com/threads/unity-on-linux-release-notes-and-known-issues.350256/
 # NOTE : You'll still have to manually install java and java-jdk
+
 # General and WebGL dependencies/recommended packages
-apt-get install gconf-service lib32gcc1 lib32stdc++6 libasound2 libc6 libc6-i386 libcairo2 libcap2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libfreetype6 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libgl1-mesa-glx libgl1 libglib2.0-0 libglu1-mesa libglu1 libgtk2.0-0 libnspr4 libnss3 libpango1.0-0 libstdc++6 libx11-6 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxtst6 zlib1g debconf npm ffmpeg libav-tools nodejs gzip
+apt-get install gconf-service lib32gcc1 lib32stdc++6 libasound2 libc6 libc6-i386 libcairo2 libcap2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libfreetype6 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libgl1-mesa-glx libglib2.0-0 libglu1-mesa libgtk2.0-0 libnspr4 libnss3 libpango1.0-0 libstdc++6 libx11-6 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxtst6 zlib1g debconf ffmpeg libav-tools nodejs gzip
